847f8a88950c741dd04641663fd52c855c06428c
Keyron13 <aguilarortizkeirodamian@gmail.com>
2023-08-08 09:18:34 -0500

    app-mediBook-angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrivateComponent } from './private/private/private.component';
+import { PublicComponent } from './public/public.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrivateComponent,
+    PublicComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

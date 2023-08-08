241c1f510a99f33b829a9d9ac8e4d977c9c07ed3
Jean10112002 <jeanpierrerodriguezzambrano@gmail.com>
2023-08-08 10:57:12 -0500

    cambios listos

--- src/app/private/private-routing.module.ts
+++ src/app/private/private-routing.module.ts
@@ -2,6 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PrivateComponent } from './private.component';
+import { CitasComponent } from './pages/citas/citas.component';
+import { EspecialidadComponent } from './pages/especialidad/especialidad.component';
+import { HorariosComponent } from './pages/horarios/horarios.component';
+import { MedicosComponent } from './pages/medicos/medicos.component';
+import { ObservacionesComponent } from './pages/observaciones/observaciones.component';
+import { PagosComponent } from './pages/pagos/pagos.component';
+import { RecetasComponent } from './pages/recetas/recetas.component';
+import { ReseniasComponent } from './pages/resenias/resenias.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { TitulosComponent } from './pages/titulos/titulos.component';
 
 
 //Components
@@ -11,7 +21,16 @@ const routes: Routes = [
   {path : '',component:PrivateComponent,children:
   [
     {path:'home', component : HomeComponent,title:'Home' },
-
+    {path:'citas', component : CitasComponent,title:'Citas' },
+    {path:'especialidad', component : EspecialidadComponent,title:'Especialidad' },
+    {path:'horarios', component : HorariosComponent,title:'Horarios' },
+    {path:'medicos', component : MedicosComponent,title:'Medicos' },
+    {path:'observaciones', component : ObservacionesComponent,title:'Observaciones' },
+    {path:'pagos', component : PagosComponent,title:'Pagos' },
+    {path:'recetas', component : RecetasComponent,title:'Recetas' },
+    {path:'resenias', component : ReseniasComponent,title:'Reseñas' },
+    {path:'reset-password', component : ResetPasswordComponent,title:'ResetPassword'},
+    {path:'titulos', component : TitulosComponent,title:'Titulos'},
   ]}
 
 
--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -5,12 +5,32 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../core/shared/shared.module';
 import { HomeComponent } from './pages/home/home.component';
 import { PrivateRoutingModule } from './private-routing.module';
+import { EspecialidadComponent } from './pages/especialidad/especialidad.component';
+import { TitulosComponent } from './pages/titulos/titulos.component';
+import { MedicosComponent } from './pages/medicos/medicos.component';
+import { HorariosComponent } from './pages/horarios/horarios.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { CitasComponent } from './pages/citas/citas.component';
+import { PagosComponent } from './pages/pagos/pagos.component';
+import { RecetasComponent } from './pages/recetas/recetas.component';
+import { ObservacionesComponent } from './pages/observaciones/observaciones.component';
+import { ReseniasComponent } from './pages/resenias/resenias.component';
 
 
 @NgModule({
   declarations: [
   
-    HomeComponent
+    HomeComponent,
+        EspecialidadComponent,
+        TitulosComponent,
+        MedicosComponent,
+        HorariosComponent,
+        ResetPasswordComponent,
+        CitasComponent,
+        PagosComponent,
+        RecetasComponent,
+        ObservacionesComponent,
+        ReseniasComponent
   ],
   imports: [
     CommonModule,HttpClientModule, FormsModule,ReactiveFormsModule,SharedModule,

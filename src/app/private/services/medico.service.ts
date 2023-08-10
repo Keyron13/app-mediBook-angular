3b9e449a551ed2a80a7624c2541bb2572918bc94
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-08-10 10:07:34 -0500

    crear titulo funcionando y roles de visualizacion

---
+++ src/app/private/services/medico.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { config } from 'src/app/config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicoService {
+  private readonly api = config.apiUrl;
+
+  constructor(private readonly http:HttpClient) { }
+  obtenerTodos(): Observable<any> {
+    return this.http.get<any>(`${this.api}medicos`);
+  }
+
+  obtenerUno(id:any): Observable<any> {
+    return this.http.get<any>(`${this.api}medicos/${id}`);
+  }
+  create(body:any):Observable<any>{
+    return this.http.post<any>(`${this.api}medicos`,body);
+  }
+  update(body:any,id:any):Observable<any>{
+    return this.http.put<any>(`${this.api}medicos/${id}`,body);
+  }
+  delete(id:any):Observable<any>{
+    return this.http.delete<any>(`${this.api}medicos/${id}`);
+  }
+}
---
+++ src/app/private/services/titulo.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { config } from 'src/app/config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TituloService {
+  private readonly api = config.apiUrl;
+
+  constructor(private readonly http:HttpClient) { }
+  obtenerTodos(): Observable<any> {
+    return this.http.get<any>(`${this.api}titulos`);
+  }
+
+  obtenerUno(id:any): Observable<any> {
+    return this.http.get<any>(`${this.api}titulos/${id}`);
+  }
+  create(body:any):Observable<any>{
+    return this.http.post<any>(`${this.api}titulos`,body);
+  }
+  update(body:any,id:any):Observable<any>{
+    return this.http.put<any>(`${this.api}titulos/${id}`,body);
+  }
+  delete(id:any):Observable<any>{
+    return this.http.delete<any>(`${this.api}tititulos/${id}`);
+  }
+}

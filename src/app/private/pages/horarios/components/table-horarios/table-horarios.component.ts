eead431869c8f7fb267a6c6a040732b73f463a6f
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-08-10 12:13:34 -0500

    listo crud de horarios

---
+++ src/app/private/pages/horarios/components/table-horarios/table-horarios.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { EventEmitterService } from 'src/app/private/services/eventEmitter.service';
+import { HorarioService } from 'src/app/private/services/horario.service';
+
+@Component({
+  selector: 'app-table-horarios',
+  templateUrl: './table-horarios.component.html',
+  styleUrls: ['./table-horarios.component.scss']
+})
+export class TableHorariosComponent {
+  @Input() horarios:any;
+  constructor(private horarioService:HorarioService,private toastService:ToastrService,private eventEmitter:EventEmitterService){
+  }
+  delete(id:any){
+    this.horarioService.delete(id).subscribe((data)=>{
+      this.toastService.success('Horario eliminado','Proceso exitoso');
+      this.eventEmitter.setEvent({event:'DELETE_HORARIO'});
+    })
+  }
+  edit(id:any){
+      this.eventEmitter.setEvent({event:'EDIT_HORARIO',id:id});
+  }
+}

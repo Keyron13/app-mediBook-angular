144725c7b178b643322e938ab01a07aab0805cb3
Janblack07 <jandryjacob20@gmail.com>
2023-08-10 00:45:26 -0500

    falta medico

--- src/app/private/pages/titulos/titulos.component.ts
+++ src/app/private/pages/titulos/titulos.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-titulos',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./titulos.component.scss']
 })
 export class TitulosComponent {
+  constructor(private readonly router: Router,
+    private formBuilder: FormBuilder,private notificacion:ToastrService,) {}
+    ngOnInit(): void {
+      this.buildForm();
+    }
+    FormTitulo!: FormGroup;
+buildForm() {
+  this.FormTitulo = this.formBuilder.group({
+    nombre: ['', [Validators.required]],
+    fecha: ['', [Validators.required]],
+    medicos: ['', [Validators.required]],
 
+  });
+}
+
+Horario() {
+  if (this.FormTitulo.invalid) {
+    // Realiza acciones si el formulario es inválido
+    return;
+  }else{
+
+  }
+
+
+
+  // Realiza acciones si todas las validaciones son exitosas
+
+}
 }

4d0b7e0535ff644bbc50ffa7287fc7b1b8b87138
Oliver-08-ojla <oliver2018es@gmail.com>
2023-08-16 14:55:01 -0500

    cambiar-password listo

--- src/app/private/pages/reset-password/components/crear-reset-password/crear-reset-password.component.ts
+++ src/app/private/pages/reset-password/components/crear-reset-password/crear-reset-password.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/public/services/auth.service';
+import { UserService } from 'src/app/private/services/user.service'; 
+import { ResetpasswordService } from 'src/app/private/services/resetpassword.service'; 
+import { EventEmitterService } from 'src/app/private/services/eventEmitter.service'; 
+
 
 @Component({
   selector: 'app-crear-reset-password',
@@ -6,5 +14,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-reset-password.component.scss']
 })
 export class CrearResetPasswordComponent {
+
+  FormResetPassword!: FormGroup;
+  users!:any
+  constructor(private readonly authService: AuthService,private router:Router,
+    private formBuilder: FormBuilder,
+    private notificacion: ToastrService,
+    private userService: UserService,
+    private resetPasswordService: ResetpasswordService,
+    private eventEmmiterService:EventEmitterService
+){
+    authService.userInformation().subscribe(data=>{
+      if(data.user.rol_id!==1){
+        this.router.navigate(['home']);
+      }
+    },()=>{
+      this.router.navigate(['home']);
+    });
+    this.buildForm();
+    /* this.users=this.userService.obtenerTodos().pipe(map(res=>res.pacientes+res.medicos)); */
+    this.userService.obtenerTodos().subscribe((data)=>{
+      this.users=[...data.pacientes, ...data.medicos];
+    });
+    this.eventEmmiterService.getEvent().subscribe(data=>{
+      if(data.event=='SELECCION_USER_RESET_PASSWORD'){
+        this.FormResetPassword.get('user_id')?.setValue(data.id)
+      }
+    })
+}
+
+onSubmit(value:any){
+  if(this.FormResetPassword.invalid){
+    this.notificacion.warning('Formulario invalido', 'Proceso erroneo');
+  }else{
+    const body={
+      "newPassword":this.FormResetPassword.get('password')?.value
+    }
+    const id=this.FormResetPassword.get('user_id')?.value
+    this.resetPasswordService.resetPassword(body,id).subscribe((data)=>{
+      this.notificacion.success('Contraseña cambiada exitosamente','Proceso exitoso');
+      this.FormResetPassword.reset()
+    })
+  }
+}
+buildForm() {
+  this.FormResetPassword = this.formBuilder.group({
+    password: ['', [Validators.required,Validators.minLength(5),Validators.maxLength(10)]],
+    user_id: ['', [Validators.required]],
+  });
+}
 
 }
--- src/app/private/pages/reset-password/components/tableusers/tableusers.component.ts
+++ src/app/private/pages/reset-password/components/tableusers/tableusers.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { EventEmitterService } from 'src/app/private/services/eventEmitter.service';
+import { CrearResetPasswordComponent } from '../crear-reset-password/crear-reset-password.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-tableusers',
@@ -9,12 +12,16 @@ import { EventEmitterService } from 'src/app/private/services/eventEmitter.servi
 export class TableusersComponent {
   term!:string;
   @Input() users:any;
-  constructor(private eventEmitterService:EventEmitterService){}
+  constructor(private eventEmitterService:EventEmitterService,  public dialog: MatDialog){}
   seleccion(user_id:any){
     console.log('emitir seleccion')
     this.eventEmitterService.setEvent({
       event:'SELECCION_USER_RESET_PASSWORD',
       id:user_id
     })
+  }
+
+   openDialog(){
+    this.dialog.open(CrearResetPasswordComponent,{width:'50%', height:'500px'});
   }
 }
